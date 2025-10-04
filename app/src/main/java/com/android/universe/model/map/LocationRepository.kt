aaa8c73d2a081ab1bc4a11b7bfb78f54b1d6a811
hanslp <hanslp@stud.ntnu.no>
2025-10-04 14:27:28 +0200

    Removed backend skeleton (Adela will implement)

--- app/src/main/java/com/android/universe/model/map/LocationRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.android.universe.model.map
-
-/**
- * Repository interface defining methods to fetch locations (local cache, database, or remote API).
- */
-class LocationRepository {
-  // TODO implement me
-}
