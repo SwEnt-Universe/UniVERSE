ce6b3d6c4f07166771189724bd34d957d45a40c1
Corentin00 <corentin.caillaud@epfl.ch>
2025-10-04 09:32:21 +0200

    feat: add UserProfile data class
    Adds the `UserProfile` data class to represent
    a user's profile information. The model includes properties for
    username, name, country, a short description, and date of birth.

---
+++ app/src/main/java/com/android/universe/model/UserProfile.kt
@@ -0,0 +1,22 @@
+package com.android.universe.model
+
+import java.time.LocalDate
+
+/**
+ * Represents a user profile in the app.
+ *
+ * @property username unique identifier chosen by the user.
+ * @property firstName given name of the user.
+ * @property lastName family name of the user.
+ * @property country country of the user (ISO 3166-1 alpha-2 code)
+ * @property description optional short bio or "about me" text.
+ * @property dateOfBirth date of birth of the user.
+ */
+data class UserProfile(
+    val username: String,
+    val firstName: String,
+    val lastName: String,
+    val country: String,
+    val description: String? = null,
+    val dateOfBirth: LocalDate
+)
