cea2808a7292b18d43791dc2aa1b5e965e19b2be
Noe.Das <noe.dassy@gmail.com>
2025-10-14 17:38:38 +0200

    docs: add tags documentation to the UserProfile

--- app/src/main/java/com/android/universe/model/user/UserProfile.kt
+++ app/src/main/java/com/android/universe/model/user/UserProfile.kt
@@ -12,6 +12,7 @@ import java.time.LocalDate
  * @property country country of the user (ISO 3166-1 alpha-2 code)
  * @property description optional short bio or "about me" text.
  * @property dateOfBirth date of birth of the user.
+ * @property tags Set of tags representing the user's interests, means of transportation, or Swiss canton.
  */
 data class UserProfile(
     val username: String,
