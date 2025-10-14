9ab56dd38792f7b71317d8de4412d9044ca4346b
Maxime Nicolas Joseph Busz <maxime.busz@epfl.ch>
2025-10-15 01:11:51 +0200

    feat: Add minimal sign-in screen

--- app/src/main/java/com/android/universe/MainActivity.kt
+++ app/src/main/java/com/android/universe/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.android.universe
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,8 +9,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTag
+import androidx.credentials.CredentialManager
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
@@ -22,7 +25,10 @@ import com.android.universe.ui.navigation.NavigationScreens
 import com.android.universe.ui.navigation.NavigationTestTags
 import com.android.universe.ui.navigation.Tab
 import com.android.universe.ui.profile.UserProfileScreen
+import com.android.universe.ui.signIn.SignInScreen
+import com.android.universe.ui.signIn.SignInViewModel
 import com.android.universe.ui.theme.SampleAppTheme
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,15 +52,31 @@ class MainActivity : ComponentActivity() {
  * This composable sets up the navigation for the app using a [NavHost].
  */
 @Composable
-fun UniverseApp() {
+fun UniverseApp(
+    context: Context = LocalContext.current,
+    credentialManager: CredentialManager = CredentialManager.create(context)
+) {
   val navController = rememberNavController()
   val navigationActions = NavigationActions(navController)
-  val startDestination = NavigationScreens.Map.route
+  val startDestination = if (FirebaseAuth.getInstance().currentUser == null) NavigationScreens.SignIn.name
+    else NavigationScreens.Map.route
   // TODO: verify that user is authenticated once the signIn is done.
 
   val onTabSelected = { tab: Tab -> navigationActions.navigateTo(tab.destination) }
 
   NavHost(navController = navController, startDestination = startDestination) {
+    navigation(
+        route = NavigationScreens.SignIn.name,
+        startDestination = NavigationScreens.SignIn.route,
+    ) {
+        composable(NavigationScreens.SignIn.route) {
+            SignInScreen(
+                viewModel = SignInViewModel(),
+                onSignedIn = {navigationActions.navigateTo(NavigationScreens.Map)},
+                credentialManager = credentialManager
+            )
+        }
+    }
     navigation(
         startDestination = NavigationScreens.Map.route,
         route = NavigationScreens.Map.name,
--- app/src/main/java/com/android/universe/ui/signIn/SignInScreen.kt
+++ app/src/main/java/com/android/universe/ui/signIn/SignInScreen.kt
@@ -2,10 +2,17 @@ package com.android.universe.ui.signIn
 
 import android.content.Context
 import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -17,12 +24,19 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.credentials.CredentialManager
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.android.universe.R
 import com.android.universe.ui.navigation.NavigationTestTags
 
 object SignInScreenTestTags {
   const val SIGN_IN_BUTTON = "signInButton"
+  const val SIGN_IN_LOGO = "signInLogo"
+  const val SIGN_IN_TITLE = "signInTitle"
 }
 
 /**
@@ -31,27 +45,70 @@ object SignInScreenTestTags {
  * @param viewModel The view model for the sign in screen.
  * @param onSignedIn The callback to be invoked when the user has signed in.
  */
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SignInScreen(
     viewModel: SignInViewModel = viewModel(),
+    credentialManager: CredentialManager = CredentialManager.create(LocalContext.current),
     onSignedIn: () -> Unit = {},
     context: Context = LocalContext.current,
 ) {
   val uiState by viewModel.uiState.collectAsState()
 
   LaunchedEffect(uiState.errorMsg) {
-    uiState.errorMsg?.let { message ->
-      Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
-      viewModel.clearErrorMsg()
-    }
+      uiState.errorMsg?.let { message ->
+          Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+          viewModel.clearErrorMsg()
+      }
   }
 
-  Scaffold(modifier = Modifier.testTag(NavigationTestTags.SIGN_IN_SCREEN)) { paddingValues ->
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.padding(paddingValues).fillMaxSize()) {
-          SignInButton(onClick = {})
+  LaunchedEffect(uiState.user) {
+      uiState.user?.let {
+          Toast.makeText(context, "Signed in successfully", Toast.LENGTH_SHORT).show()
+          onSignedIn()
+      }
+  }
+
+  Scaffold(
+      modifier = Modifier
+          .testTag(NavigationTestTags.SIGN_IN_SCREEN)
+          .fillMaxSize()
+  ) {
+      paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Image(
+                painter = painterResource(R.drawable.app_logo_placeholder),
+                contentDescription = "App Logo",
+                modifier = Modifier
+                    .testTag(SignInScreenTestTags.SIGN_IN_LOGO)
+                    .size(256.dp)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                modifier = Modifier
+                    .testTag(SignInScreenTestTags.SIGN_IN_TITLE),
+                text = "Welcome to UNIVerse !",
+                style = MaterialTheme.typography.titleLarge,
+                textAlign = TextAlign.Center
+            )
+
+            if (uiState.isLoading){
+                LinearProgressIndicator()
+            } else
+                SignInButton(onClick = {
+                    viewModel.signIn(
+                        context = context,
+                        credentialManager = credentialManager
+                    )
+                })
         }
   }
 }
