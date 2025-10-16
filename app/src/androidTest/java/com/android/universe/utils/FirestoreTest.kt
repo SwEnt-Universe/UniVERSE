2c34345657f2a421f89eccf7554909e32f4c4e54
Noe.Das <noe.dassy@gmail.com>
2025-10-16 18:08:24 +0200

    feat: add base class for Firestore tests
    Introduce `FirestoreTest`, a base class for instrumented tests that interact with the Firestore emulator.
    
    This class includes:
    - Setup (`@Before`) and teardown (`@After`) logic.
    - A dispatcher (`testDispatcher`) for coroutines.
    - Helper methods to get the user count and clear the test collection.
    - A factory method (`createInitializedRepository`) for creating a `UserRepository`.

---
+++ app/src/androidTest/java/com/android/universe/utils/FirestoreTest.kt
@@ -0,0 +1,62 @@
+package com.android.universe.utils
+
+import android.util.Log
+import com.android.universe.model.user.USERS_COLLECTION_PATH
+import com.android.universe.model.user.UserRepository
+import com.android.universe.model.user.UserRepositoryFirestore
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Before
+import java.net.HttpURLConnection
+import java.net.URL
+
+open class FirestoreTest() {
+    val testDispatcher = StandardTestDispatcher()
+    val emulator = FirebaseEmulator
+
+    suspend fun getUserCount(): Int {
+        return emulator.firestore.collection(USERS_COLLECTION_PATH).get().await().size()
+    }
+
+    private suspend fun clearTestCollection() {
+        val todos = emulator.firestore.collection(USERS_COLLECTION_PATH).get().await()
+
+        val batch = emulator.firestore.batch()
+        todos.documents.forEach { batch.delete(it.reference) }
+        batch.commit().await()
+
+        assert(getUserCount() == 0) {
+            "Test collection is not empty after clearing, count: ${getUserCount()}"
+        }
+    }
+
+    fun createInitializedRepository(): UserRepository {
+        return UserRepositoryFirestore(db = emulator.firestore)
+    }
+
+    @Before
+    open fun setUp() {
+        val url = URL("http://10.0.2.2:8080") // Firestore emulator host for Android
+        val connection = url.openConnection() as HttpURLConnection
+        connection.connectTimeout = 2000
+        connection.requestMethod = "GET"
+        runTest {
+            val userCount = getUserCount()
+            if (userCount > 0) {
+                Log.w(
+                    "FirebaseEmulatedTest",
+                    "Warning: Test collection is not empty at the beginning of the test, count: $userCount",
+                )
+                clearTestCollection()
+            }
+        }
+    }
+
+    @After
+    open fun tearDown() {
+        runTest { //clearTestCollection()
+        }
+    }
+}
