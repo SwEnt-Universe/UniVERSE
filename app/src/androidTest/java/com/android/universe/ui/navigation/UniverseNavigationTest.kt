7fc86e161c4101fdb6f48cb0ae3fff9aa4ca9010
Maxime Nicolas Joseph Busz <maxime.busz@epfl.ch>
2025-10-25 13:55:36 +0200

    refactor: Organize imports and update tests

--- app/src/androidTest/java/com/android/universe/ui/navigation/UniverseNavigationTest.kt
+++ app/src/androidTest/java/com/android/universe/ui/navigation/UniverseNavigationTest.kt
@@ -12,6 +12,7 @@ import com.android.universe.model.user.UserProfile
 import com.android.universe.model.user.UserRepositoryProvider
 import com.android.universe.ui.profile.UserProfileScreenTestTags
 import com.android.universe.utils.FirebaseEmulator
+import java.time.LocalDate
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.test.runTest
@@ -20,7 +21,6 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.time.LocalDate
 
 @OptIn(ExperimentalCoroutinesApi::class)
 @RunWith(AndroidJUnit4::class)
