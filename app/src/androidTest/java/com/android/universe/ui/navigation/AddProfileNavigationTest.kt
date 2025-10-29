c677c1173e787ef47a9c35b9e16223b277ba5de3
hanslp <hanslp@stud.ntnu.no>
2025-10-29 21:35:38 +0100

    ktfmtFormat

---
+++ app/src/androidTest/java/com/android/universe/ui/navigation/AddProfileNavigationTest.kt
@@ -0,0 +1,51 @@
+package com.android.universe.ui.navigation
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.android.universe.ui.profileCreation.AddProfileScreen
+import com.android.universe.ui.profileCreation.AddProfileScreenTestTags
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class AddProfileNavigationTest {
+
+  @get:Rule val composeTestRule = createComposeRule()
+
+  @Test
+  fun addProfileScreen_backButton_navigatesToSignIn() {
+    composeTestRule.setContent {
+      val navController = rememberNavController()
+      NavHost(
+          navController = navController, startDestination = NavigationScreens.AddProfile.route) {
+            composable(NavigationScreens.AddProfile.route) {
+              AddProfileScreen(
+                  uid = "test_uid",
+                  onBack = {
+                    navController.navigate(NavigationScreens.SignIn.route) {
+                      popUpTo(NavigationScreens.AddProfile.route) { inclusive = true }
+                    }
+                  })
+            }
+            composable(NavigationScreens.SignIn.route) {
+              Box(Modifier.testTag(NavigationTestTags.SIGN_IN_SCREEN)) { Text("Sign In Screen") }
+            }
+          }
+    }
+
+    composeTestRule.onNodeWithTag(NavigationTestTags.ADD_PROFILE_SCREEN).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(AddProfileScreenTestTags.BACK_BUTTON).performClick()
+    composeTestRule.onNodeWithTag(NavigationTestTags.SIGN_IN_SCREEN).assertIsDisplayed()
+  }
+}
---
+++ app/src/test/java/com/android/universe/ui/navigation/NavigationMainActivity.kt
@@ -0,0 +1,3 @@
+package com.android.universe.ui.navigation
+
+class NavigationMainActivity {}
