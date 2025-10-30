0ba582aceee0f3b5001233c7775e14eb2d07ff98
AdeSolorio <a01637205@tec.mx>
2025-10-30 15:36:13 +0100

    Change Firebase emulator setup on CI

--- app/src/testFixtures/java/com.android.universe/utils/FirebaseEmulator.kt
+++ app/src/testFixtures/java/com.android.universe/utils/FirebaseEmulator.kt
@@ -29,21 +29,24 @@ object FirebaseEmulator {
   val firestore: FirebaseFirestore
     get() = Firebase.firestore
 
+  val hostFromEnv: String? = System.getenv("FIREBASE_EMULATOR_HOST")
+
   val currentHost: String
     get() =
-        if (isConnected) {
-          val projectId =
-              try {
-                auth.app.options.projectId ?: ""
-              } catch (_: IllegalStateException) {
-                ""
-              }
+        hostFromEnv
+            ?: if (isConnected) {
+              val projectId =
+                  try {
+                    auth.app.options.projectId ?: ""
+                  } catch (_: IllegalStateException) {
+                    ""
+                  }
 
-          if (projectId.contains("robolectric", ignoreCase = true)) ROBOLECTRIC_HOST
-          else ANDROID_EMULATOR_HOST
-        } else {
-          ANDROID_EMULATOR_HOST
-        }
+              if (projectId.contains("robolectric", ignoreCase = true)) ROBOLECTRIC_HOST
+              else ANDROID_EMULATOR_HOST
+            } else {
+              ANDROID_EMULATOR_HOST
+            }
 
   /**
    * Connects the Firebase instance to the local emulators. This MUST be called after
