ea5052985b48162bdcc4bdcbee6235b965b26f0a
poujhh <implicitfunctiontheorem@gmail.com>
2025-11-06 16:45:33 +0100

    feat: add event creation screen
    Adds a new `EventCreationScreen` to the navigation graph.
    
    This screen is accessed from the `MapScreen` by passing the latitude and
    longitude as navigation arguments. After saving, the user is navigated
    back to the map.

--- app/src/main/java/com/android/universe/MainActivity.kt
+++ app/src/main/java/com/android/universe/MainActivity.kt
@@ -29,9 +29,11 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.android.universe.model.location.Location
 import com.android.universe.model.user.UserRepositoryProvider
 import com.android.universe.resources.C
 import com.android.universe.ui.event.EventScreen
+import com.android.universe.ui.eventCreation.EventCreationScreen
 import com.android.universe.ui.map.MapScreen
 import com.android.universe.ui.navigation.NavigationActions
 import com.android.universe.ui.navigation.NavigationPlaceholderScreen
@@ -138,7 +140,7 @@ fun UniverseApp(
           startDestination = NavigationScreens.Map.route,
           route = NavigationScreens.Map.name,
       ) {
-        composable(NavigationScreens.Map.route) { MapScreen(onTabSelected) }
+        composable(NavigationScreens.Map.route) { MapScreen(onTabSelected, createEvent = { lat, lng -> navController.navigate("eventCreation/$lat/$lng") }) }
       }
 
       navigation(
@@ -189,6 +191,24 @@ fun UniverseApp(
                   credentialManager.clearCredentialState(request = ClearCredentialStateRequest())
                 })
           }
+        navigation(
+            startDestination = NavigationScreens.EventCreation.route,
+            route = NavigationScreens.EventCreation.name,
+        ) {
+            composable(
+                route = NavigationScreens.EventCreation.route,
+                arguments =
+                    listOf(
+                        navArgument("latitude") { type = NavType.FloatType },
+                        navArgument("longitude") { type = NavType.FloatType })) { backStackEntry ->
+                val latitude = backStackEntry.arguments?.getFloat("latitude") ?: 0.0
+                val longitude = backStackEntry.arguments?.getFloat("longitude") ?: 0.0
+
+                EventCreationScreen(
+                    location = Location(latitude.toDouble(), longitude.toDouble()),
+                    onSave = { navController.navigate(NavigationScreens.Map.route) })
+            }
+        }
     }
   }
 }
