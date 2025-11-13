e5c7f0af54865861139261efef3773b74f2892c0
Corentin00 <corentin.caillaud@epfl.ch>
2025-11-13 16:33:42 +0100

    chore: Add license headers and component tests
    Adds the Apache 2.0 license header to several UI component files.
    It also introduces unit tests for the `LiquidBottomTab` composable to
    verify click handling and consumption of `LocalLiquidBottomTabScale`.

--- app/src/main/java/com/android/universe/ui/components/LiquidBottomTab.kt
+++ app/src/main/java/com/android/universe/ui/components/LiquidBottomTab.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 The AndroidLiquidGlass Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /**
  * Original source:
  * https://github.com/Kyant0/AndroidLiquidGlass/blob/master/catalog/src/main/java/com/kyant/backdrop/catalog/components/LiquidBottomTab.kt
@@ -42,7 +58,7 @@ internal val LocalLiquidBottomTabScale = staticCompositionLocalOf { { 1f } }
 
 /**
  * A custom, liquid-animated tab item designed for use within a horizontal layout, typically a
- * [NavigationBar] or [Row].
+ * [NavigationBar].
  *
  * This composable enforces its placement within a [RowScope] (via the extension function syntax)
  * and uses a custom scale value provided by [LocalLiquidBottomTabScale] to achieve a dynamic
