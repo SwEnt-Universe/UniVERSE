d81083fee8e9a2a6437beb343aaaace7fdddaf91
hanslp <hanslp@stud.ntnu.no>
2025-11-20 17:10:34 +0100

    tests: added one, coverage check

---
+++ app/src/test/java/com/android/universe/ui/search/SearchEngineTest.kt
@@ -0,0 +1,25 @@
+package com.android.universe.ui.search
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class SearchEngineTest {
+
+	@Test
+	fun fuzzyMatch_exactMatchReturnsTrue() {
+		val result = SearchEngine.fuzzyMatch("concert", "concert")
+		assertEquals(true, result)
+	}
+
+	@Test
+	fun fuzzyMatch_smallDifferencesReturnTrue() {
+		val result = SearchEngine.fuzzyMatch("concert", "concerrt")
+		assertEquals(true, result)
+	}
+
+	@Test
+	fun fuzzyMatch_largeDifferencesReturnFalse() {
+		val result = SearchEngine.fuzzyMatch("concert", "basketball")
+		assertEquals(false, result)
+	}
+}
