a1adae032747fb0655fad527986be30454fe91e5
Noe.Das <noe.dassy@gmail.com>
2025-11-20 19:23:31 +0100

    refactor: remove unused import in TagItem
    Remove the unused `androidx.compose.ui.unit.sp` import from the `TagItem.kt` file.

--- app/src/main/java/com/android/universe/ui/components/TagItem.kt
+++ app/src/main/java/com/android/universe/ui/components/TagItem.kt
@@ -20,7 +20,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.android.universe.model.tag.Tag
 import com.android.universe.ui.theme.CapsuleLarge
 import com.android.universe.ui.theme.Dimensions
