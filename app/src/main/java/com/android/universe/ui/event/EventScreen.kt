a5f89164143e915fc117b58d6e72f0095f6a9f7b
hanslp <hanslp@stud.ntnu.no>
2025-11-20 16:58:03 +0100

    add: focusManager for searchbar states

--- app/src/main/java/com/android/universe/ui/event/EventScreen.kt
+++ app/src/main/java/com/android/universe/ui/event/EventScreen.kt
@@ -5,6 +5,8 @@ import android.graphics.BitmapFactory
 import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -27,12 +29,14 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.produceState
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -120,8 +124,19 @@ fun EventScreen(
     }
   }
   val events by viewModel.filteredEvents.collectAsState()
+  val focusManager = LocalFocusManager.current
 
-  Column(modifier = Modifier.fillMaxSize().padding(horizontal = PaddingMedium)) {
+  Column(
+    modifier = Modifier
+      .fillMaxSize()
+      .padding(horizontal = PaddingMedium)
+      .clickable(
+        indication = null,
+        interactionSource = remember { MutableInteractionSource() }
+      ) {
+        focusManager.clearFocus()
+      }
+  ) {
     SearchBar(
         query = viewModel.searchQuery.collectAsState().value,
         onQueryChange = viewModel::updateSearchQuery,
