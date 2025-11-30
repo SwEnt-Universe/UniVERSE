d36f3ac86aee21e4048d89bef5f5b51ceda63047
Corentin00 <corentin.caillaud@epfl.ch>
2025-11-30 18:48:12 +0100

    feat: Add delete functionality LiquidImagePicker
    Updates the `LiquidImagePicker` component to support removing a
    selected image.
    - Adds an optional `onDeleteImage` callback parameter.
    - Displays a delete button with a trash icon when an image is present.
    - Adjusts layout and padding for the edit/add and delete buttons within
    the component.

--- app/src/main/java/com/android/universe/ui/components/LiquidImagePicker.kt
+++ app/src/main/java/com/android/universe/ui/components/LiquidImagePicker.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.outlined.Image
 import androidx.compose.material3.Icon
@@ -43,6 +44,7 @@ fun LiquidImagePicker(
     imageBytes: ByteArray?,
     onPickImage: () -> Unit,
     modifier: Modifier = Modifier,
+    onDeleteImage: () -> Unit = {},
     dispatcher: CoroutineDispatcher = DefaultDP.default
 ) {
   val imageBitmap by
@@ -51,18 +53,17 @@ fun LiquidImagePicker(
       }
 
   LiquidBox(shape = RoundedCornerShape(24.dp), modifier = modifier) {
-    // We use that Box with fillMaxSize to manage the alignment of inner children
-    Box(modifier = Modifier.fillMaxSize()) {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
       Box(
           modifier =
               Modifier.matchParentSize()
-                  .padding(Dimensions.PaddingLarge)
+                  .padding(Dimensions.PaddingMedium)
                   .clip(RoundedCornerShape(16.dp)),
           contentAlignment = Alignment.Center) {
             if (imageBitmap != null) {
               Image(
                   bitmap = imageBitmap!!,
-                  contentDescription = "Image",
+                  contentDescription = "Selected Image",
                   contentScale = ContentScale.Crop,
                   modifier = Modifier.fillMaxSize())
             } else {
@@ -74,12 +75,35 @@ fun LiquidImagePicker(
             }
           }
 
+      if (imageBitmap != null) {
+        LiquidButton(
+            onClick = onDeleteImage,
+            width = 32f,
+            height = 32f,
+            contentPadding = Dimensions.PaddingSmall,
+            modifier =
+                Modifier.align(Alignment.BottomStart)
+                    .padding(
+                        start = Dimensions.PaddingMedium + 4.dp,
+                        bottom = Dimensions.PaddingMedium + 4.dp)) {
+              Icon(
+                  imageVector = Icons.Default.Delete,
+                  contentDescription = "Delete Image",
+                  tint = MaterialTheme.colorScheme.onBackground,
+                  modifier = Modifier.size(24.dp))
+            }
+      }
+
       LiquidButton(
           onClick = onPickImage,
           width = 32f,
           height = 32f,
           contentPadding = Dimensions.PaddingSmall,
-          modifier = Modifier.align(Alignment.BottomEnd).padding(Dimensions.PaddingMedium)) {
+          modifier =
+              Modifier.align(Alignment.BottomEnd)
+                  .padding(
+                      end = Dimensions.PaddingMedium + 4.dp,
+                      bottom = Dimensions.PaddingMedium + 4.dp)) {
             Icon(
                 imageVector = if (imageBitmap != null) Icons.Default.Edit else Icons.Default.Add,
                 contentDescription = if (imageBitmap != null) "Edit Image" else "Add Image",
