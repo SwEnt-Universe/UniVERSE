5e1e83ab4a3106774674511c47dc9114306161b0
Corentin00 <corentin.caillaud@epfl.ch>
2025-12-02 00:02:41 +0100

    test: Rewrite ImageDisplayTest

--- app/src/test/java/com/android/universe/ui/components/ImageDisplayTest.kt
+++ app/src/test/java/com/android/universe/ui/components/ImageDisplayTest.kt
@@ -1,149 +1,114 @@
 package com.android.universe.ui.components
 
-import android.content.Context
-import android.net.Uri
-import androidx.test.core.app.ApplicationProvider
-import com.android.universe.di.DispatcherProvider
-import com.android.universe.model.image.ImageBitmapManager
-import com.android.universe.utils.MainCoroutineRule
-import java.io.ByteArrayInputStream
-import junit.framework.TestCase.assertNotNull
-import junit.framework.TestCase.assertNull
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.runTest
-import org.junit.Before
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
-import org.robolectric.Shadows
 
-@OptIn(ExperimentalCoroutinesApi::class)
 @RunWith(RobolectricTestRunner::class)
 class ImageDisplayTest {
 
-  @get:Rule val mainCoroutineRule = MainCoroutineRule()
+    @get:Rule val composeTestRule = createComposeRule()
 
-  private lateinit var context: Context
-  private lateinit var uri: Uri
-  private lateinit var imageBitmapManager: ImageBitmapManager
+    private val validPngBytes =
+        byteArrayOf(
+            0x89.toByte(),
+            0x50.toByte(),
+            0x4E.toByte(),
+            0x47.toByte(),
+            0x0D.toByte(),
+            0x0A.toByte(),
+            0x1A.toByte(),
+            0x0A.toByte(),
+            0x00.toByte(),
+            0x00.toByte(),
+            0x00.toByte(),
+            0x0D.toByte(),
+            0x49.toByte(),
+            0x48.toByte(),
+            0x44.toByte(),
+            0x52.toByte(),
+            0x00.toByte(),
+            0x00.toByte(),
+            0x00.toByte(),
+            0x01.toByte(),
+            0x00.toByte(),
+            0x00.toByte(),
+            0x00.toByte(),
+            0x01.toByte(),
+            0x08.toByte(),
+            0x02.toByte(),
+            0x00.toByte(),
+            0x00.toByte(),
+            0x00.toByte(),
+            0x90.toByte(),
+            0x77.toByte(),
+            0x53.toByte(),
+            0xDE.toByte(),
+            0x00.toByte(),
+            0x00.toByte(),
+            0x00.toByte(),
+            0x0C.toByte(),
+            0x49.toByte(),
+            0x44.toByte(),
+            0x41.toByte(),
+            0x54.toByte(),
+            0x08.toByte(),
+            0xD7.toByte(),
+            0x63.toByte(),
+            0xF8.toByte(),
+            0xCF.toByte(),
+            0xC0.toByte(),
+            0x00.toByte(),
+            0x00.toByte(),
+            0x03.toByte(),
+            0x01.toByte(),
+            0x01.toByte(),
+            0x00.toByte(),
+            0x18.toByte(),
+            0xDD.toByte(),
+            0x8D.toByte(),
+            0xB0.toByte(),
+            0x00.toByte(),
+            0x00.toByte(),
+            0x00.toByte(),
+            0x00.toByte(),
+            0x49.toByte(),
+            0x45.toByte(),
+            0x4E.toByte(),
+            0x44.toByte(),
+            0xAE.toByte(),
+            0x42.toByte(),
+            0x60.toByte(),
+            0x82.toByte())
 
-  @Before
-  fun setup() {
-    context = ApplicationProvider.getApplicationContext()
-    uri = Uri.parse("content://com.android.universe/test_image.png")
+    @Test
+    fun imageDisplay_showsDefaultImage_whenImageIsNull() {
+        val defaultImageId = android.R.drawable.ic_menu_camera
+        val contentDesc = "Default Profile Picture"
 
-    val testDispatcher = mainCoroutineRule.dispatcher
-
-    val testDispatcherProvider =
-        object : DispatcherProvider {
-          override val main: CoroutineDispatcher
-            get() = testDispatcher
-
-          override val default: CoroutineDispatcher
-            get() = testDispatcher
-
-          override val io: CoroutineDispatcher
-            get() = testDispatcher
-
-          override val unconfined: CoroutineDispatcher
-            get() = testDispatcher
+        composeTestRule.setContent {
+            ImageDisplay(image = null, defaultImageId = defaultImageId, contentDescription = contentDesc)
         }
 
-    imageBitmapManager = ImageBitmapManager(context, testDispatcherProvider)
-  }
+        composeTestRule.onNodeWithContentDescription(contentDesc).assertIsDisplayed()
+    }
 
-  @Test
-  fun resizeAndCompressImage_returnsNull_whenInputStreamCannotBeOpened() =
-      runTest(mainCoroutineRule.dispatcher) {
-        val missingUri = Uri.parse("file:///android_asset/does_not_exist.png")
+    @Test
+    fun imageDisplay_showsBitmap_whenImageIsNotNull() {
+        val defaultImageId = android.R.drawable.ic_menu_camera
+        val contentDesc = "User Profile Picture"
 
-        val result = imageBitmapManager.resizeAndCompressImage(missingUri)
+        composeTestRule.setContent {
+            ImageDisplay(
+                image = validPngBytes, defaultImageId = defaultImageId, contentDescription = contentDesc)
+        }
 
-        assertNull(result)
-      }
+        composeTestRule.waitForIdle()
 
-  @Test
-  fun resizeAndCompressImage_returnsByteArray_whenImageIsValid() =
-      runTest(mainCoroutineRule.dispatcher) {
-        val validPngBytes =
-            byteArrayOf(
-                0x89.toByte(),
-                0x50.toByte(),
-                0x4E.toByte(),
-                0x47.toByte(),
-                0x0D.toByte(),
-                0x0A.toByte(),
-                0x1A.toByte(),
-                0x0A.toByte(),
-                0x00.toByte(),
-                0x00.toByte(),
-                0x00.toByte(),
-                0x0D.toByte(),
-                0x49.toByte(),
-                0x48.toByte(),
-                0x44.toByte(),
-                0x52.toByte(),
-                0x00.toByte(),
-                0x00.toByte(),
-                0x00.toByte(),
-                0x01.toByte(),
-                0x00.toByte(),
-                0x00.toByte(),
-                0x00.toByte(),
-                0x01.toByte(),
-                0x08.toByte(),
-                0x02.toByte(),
-                0x00.toByte(),
-                0x00.toByte(),
-                0x00.toByte(),
-                0x90.toByte(),
-                0x77.toByte(),
-                0x53.toByte(),
-                0xDE.toByte(),
-                0x00.toByte(),
-                0x00.toByte(),
-                0x00.toByte(),
-                0x0C.toByte(),
-                0x49.toByte(),
-                0x44.toByte(),
-                0x41.toByte(),
-                0x54.toByte(),
-                0x08.toByte(),
-                0xD7.toByte(),
-                0x63.toByte(),
-                0xF8.toByte(),
-                0xCF.toByte(),
-                0xC0.toByte(),
-                0x00.toByte(),
-                0x00.toByte(),
-                0x03.toByte(),
-                0x01.toByte(),
-                0x01.toByte(),
-                0x00.toByte(),
-                0x18.toByte(),
-                0xDD.toByte(),
-                0x8D.toByte(),
-                0xB0.toByte(),
-                0x00.toByte(),
-                0x00.toByte(),
-                0x00.toByte(),
-                0x00.toByte(),
-                0x49.toByte(),
-                0x45.toByte(),
-                0x4E.toByte(),
-                0x44.toByte(),
-                0xAE.toByte(),
-                0x42.toByte(),
-                0x60.toByte(),
-                0x82.toByte())
-
-        val inputStream = ByteArrayInputStream(validPngBytes)
-        Shadows.shadowOf(context.contentResolver).registerInputStream(uri, inputStream)
-
-        val result = imageBitmapManager.resizeAndCompressImage(uri)
-
-        assertNotNull("Result should not be null for valid image", result)
-      }
+        composeTestRule.onNodeWithContentDescription(contentDesc).assertIsDisplayed()
+    }
 }
