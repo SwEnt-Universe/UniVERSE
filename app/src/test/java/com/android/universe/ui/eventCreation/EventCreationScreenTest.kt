46bd82aa7c7ec34b9334b899d576ab0d25a55e2f
Noe.Das <noe.dassy@gmail.com>
2025-12-02 15:46:22 +0100

    refactor: Update EventCreationScreen tests
    Refactor the tests for the `EventCreationScreen`.
    Remove several outdated tests related to error states, tag handling, and UI elements that have been changed.
    Update the test for entering time to use `performTextInput` on the text field instead of interacting with a time picker dialog.
    Update the date entry test to click the text field to open the date picker.
    Adjust the main display test to assert the presence of new and changed components.

--- app/src/test/java/com/android/universe/ui/eventCreation/EventCreationScreenTest.kt
+++ app/src/test/java/com/android/universe/ui/eventCreation/EventCreationScreenTest.kt
@@ -1,29 +1,22 @@
 package com.android.universe.ui.eventCreation
 
-import androidx.compose.ui.test.assertAny
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsNotDisplayed
 import androidx.compose.ui.test.assertTextContains
-import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onAllNodesWithTag
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
-import androidx.compose.ui.test.performTextReplacement
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.android.universe.model.event.FakeEventRepository
 import com.android.universe.model.location.Location
 import com.android.universe.model.tag.Tag
 import com.android.universe.utils.nextMonth
 import com.android.universe.utils.pressOKDate
-import com.android.universe.utils.pressOKTime
-import com.android.universe.utils.previousMonth
 import com.android.universe.utils.selectDay
-import com.android.universe.utils.selectHour
-import com.android.universe.utils.selectMinute
 import com.android.universe.utils.selectYear
+import com.android.universe.utils.setContentWithStubBackdrop
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import org.junit.Before
@@ -37,20 +30,19 @@ class EventCreationScreenTest {
   @get:Rule val composeTestRule = createComposeRule()
 
   companion object {
-    const val SAMPLE_ERASED_TEXT = ""
     const val SAMPLE_TEXT_INPUT = "Text"
     const val SAMPLE_MINUTE = 25
     const val SAMPLE_HOUR = 17
     const val FORMATTED_TIME = "$SAMPLE_HOUR:$SAMPLE_MINUTE"
     const val SAMPLE_YEAR = 2027
     const val SAMPLE_DAY = 17
-    val SAMPLE_DATE = LocalDate.of(SAMPLE_YEAR, LocalDate.now().month, SAMPLE_DAY)
+    val SAMPLE_DATE: LocalDate = LocalDate.of(SAMPLE_YEAR, LocalDate.now().month, SAMPLE_DAY)
   }
 
   @Before
   fun setUp() {
     viewModel = EventCreationViewModel(eventRepository = FakeEventRepository())
-    composeTestRule.setContent {
+    composeTestRule.setContentWithStubBackdrop {
       EventCreationScreen(
           eventCreationViewModel = viewModel,
           location = Location(0.0, 0.0),
@@ -89,23 +81,18 @@ class EventCreationScreenTest {
 
   @Test
   fun eventCreationScreen_displayedCorrectly() {
+    composeTestRule.onNodeWithTag(EventCreationTestTags.EVENT_PICTURE_PICKER).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(EventCreationTestTags.CREATION_EVENT_TITLE).assertIsDisplayed()
+    composeTestRule
+        .onNodeWithTag(EventCreationTestTags.AI_BUTTON, useUnmergedTree = true)
+        .assertIsDisplayed()
     composeTestRule.onNodeWithTag(EventCreationTestTags.EVENT_TITLE_TEXT_FIELD).assertIsDisplayed()
     composeTestRule
         .onNodeWithTag(EventCreationTestTags.EVENT_DESCRIPTION_TEXT_FIELD)
         .assertIsDisplayed()
-
-    composeTestRule.onNodeWithTag(EventCreationTestTags.ERROR_TITLE).assertIsNotDisplayed()
-    composeTestRule.onNodeWithTag(EventCreationTestTags.ERROR_DESCRIPTION).assertIsNotDisplayed()
-
-    composeTestRule.onNodeWithTag(EventCreationTestTags.ADD_TAG_BUTTON).assertIsDisplayed()
-    composeTestRule.onNodeWithTag(EventCreationTestTags.TAG).assertIsNotDisplayed()
-
-    composeTestRule.onNodeWithTag(EventCreationTestTags.SAVE_EVENT_BUTTON).assertExists()
-
-    composeTestRule.onNodeWithTag(EventCreationTestTags.IMAGE_EVENT).assertIsNotDisplayed()
-    composeTestRule.onNodeWithTag(EventCreationTestTags.IMAGE_ICON).assertIsDisplayed()
-    composeTestRule.onNodeWithTag(EventCreationTestTags.EDIT_IMAGE_ICON).assertIsDisplayed()
-    composeTestRule.onNodeWithTag(EventCreationTestTags.DELETE_IMAGE_BUTTON).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(EventCreationTestTags.EVENT_DATE_TEXT_FIELD).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(EventCreationTestTags.EVENT_DATE_PICKER).assertIsNotDisplayed()
+    composeTestRule.onNodeWithTag(EventCreationTestTags.EVENT_TIME_TEXT_FIELD).assertIsDisplayed()
   }
 
   @Test
@@ -131,84 +118,26 @@ class EventCreationScreenTest {
   @Test
   fun eventCreationScreen_canEnterTime() {
 
-    composeTestRule.onNodeWithTag(EventCreationTestTags.TIME_BUTTON).performClick()
+    composeTestRule
+        .onNodeWithTag(EventCreationTestTags.EVENT_TIME_TEXT_FIELD)
+        .performTextInput(FORMATTED_TIME)
     composeTestRule.waitForIdle()
-    composeTestRule.onNodeWithTag(EventCreationTestTags.TIME_DIALOG).assertIsDisplayed()
-    composeTestRule.waitForIdle()
-
-    selectHour(composeTestRule, SAMPLE_HOUR)
-
-    selectMinute(composeTestRule, SAMPLE_MINUTE)
-    pressOKTime(composeTestRule)
-    composeTestRule.onNodeWithText(FORMATTED_TIME).assertIsDisplayed()
+    composeTestRule
+        .onNodeWithTag(EventCreationTestTags.EVENT_TIME_TEXT_FIELD)
+        .assertTextContains(FORMATTED_TIME)
   }
 
   @Test
   fun eventCreationScreen_canEnterDate() {
     val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
-    composeTestRule.onNodeWithTag(EventCreationTestTags.DATE_BUTTON).performClick()
+    composeTestRule.onNodeWithTag(EventCreationTestTags.EVENT_DATE_TEXT_FIELD).performClick()
     composeTestRule.waitForIdle()
-    composeTestRule.onNodeWithTag(EventCreationTestTags.DATE_DIALOG).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(EventCreationTestTags.EVENT_DATE_PICKER).assertIsDisplayed()
     selectYear(composeTestRule, SAMPLE_YEAR)
     nextMonth(composeTestRule)
     composeTestRule.waitForIdle()
     selectDay(composeTestRule, SAMPLE_DATE)
     pressOKDate(composeTestRule)
     // composeTestRule.onNodeWithText(formatter.format(SAMPLE_DATE)).assertIsDisplayed()
-  }
-
-  @Test
-  fun pastDatePutsError() {
-    composeTestRule.onNodeWithTag(EventCreationTestTags.DATE_BUTTON).performClick()
-    composeTestRule.waitForIdle()
-    composeTestRule.onNodeWithTag(EventCreationTestTags.DATE_DIALOG).assertIsDisplayed()
-    previousMonth(composeTestRule)
-    composeTestRule.waitForIdle()
-    selectDay(composeTestRule, SAMPLE_DATE.minusMonths(1))
-
-    pressOKDate(composeTestRule)
-    composeTestRule
-        .onNodeWithTag(EventCreationTestTags.ERROR_DATE, useUnmergedTree = true)
-        .assertIsDisplayed()
-  }
-
-  @Test
-  fun eventCreationScreen_TitleEmpty_Display_Error() {
-    composeTestRule
-        .onNodeWithTag(EventCreationTestTags.EVENT_TITLE_TEXT_FIELD)
-        .performTextInput(SAMPLE_TEXT_INPUT)
-    composeTestRule
-        .onNodeWithTag(EventCreationTestTags.EVENT_TITLE_TEXT_FIELD)
-        .performTextReplacement("")
-    composeTestRule
-        .onNodeWithTag(EventCreationTestTags.ERROR_TITLE, useUnmergedTree = true)
-        .assertIsDisplayed()
-  }
-
-  @Test
-  fun eventCreationScreen_DescriptionEmpty_NotDisplay_Error() {
-    composeTestRule
-        .onNodeWithTag(EventCreationTestTags.EVENT_DESCRIPTION_TEXT_FIELD)
-        .performTextInput(SAMPLE_TEXT_INPUT)
-    composeTestRule
-        .onNodeWithTag(EventCreationTestTags.EVENT_DESCRIPTION_TEXT_FIELD)
-        .performTextReplacement(SAMPLE_ERASED_TEXT)
-    composeTestRule
-        .onNodeWithTag(EventCreationTestTags.ERROR_DESCRIPTION, useUnmergedTree = true)
-        .assertIsNotDisplayed()
-  }
-
-  @Test
-  fun eventCreationScreen_CanAddTags() {
-    composeTestRule.onNodeWithTag(EventCreationTestTags.ADD_TAG_BUTTON).performClick()
-    composeTestRule
-        .onAllNodesWithTag(EventCreationTestTags.TAG)
-        .assertAny(hasTestTag(EventCreationTestTags.TAG))
-  }
-
-  @Test
-  fun eventCreationScreen_AddTags_SaveButtonDisplay() {
-    composeTestRule.onNodeWithTag(EventCreationTestTags.ADD_TAG_BUTTON).performClick()
-    composeTestRule.onNodeWithTag(EventCreationTestTags.SAVE_EVENT_BUTTON).assertExists()
   }
 }
