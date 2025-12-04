e6ed3f87ca467dbde601698933937fe873869d49
Raphael <raphael@heynet.ch>
2025-12-04 10:57:14 +0100

    refactor: Remove explicit `contentWindowInsets`
    Removes the explicit `contentWindowInsets = WindowInsets(0, 0, 0, 0)`
    from the `ScreenLayout` `Scaffold`. This allows the default system
    window insets to be passed into the `scaffoldPadding`
    
    This change also adjusts the top padding in `ChatListScreen` to use
    `paddingValues.calculateTopPadding()` to respect the insets provided
    by the `ScreenLayout`.

--- app/src/main/java/com/android/universe/ui/components/ScreenLayout.kt
+++ app/src/main/java/com/android/universe/ui/components/ScreenLayout.kt
@@ -3,7 +3,6 @@ package com.android.universe.ui.components
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
@@ -39,7 +38,6 @@ fun ScreenLayout(
   Scaffold(
       modifier = modifier,
       containerColor = Color.Transparent,
-      contentWindowInsets = WindowInsets(0, 0, 0, 0),
       topBar = topBar,
       bottomBar = bottomBar,
   ) { scaffoldPadding ->
