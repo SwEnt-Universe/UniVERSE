f08f438c2dc0b46a8b4b341ec53b09f2c3502c53
Raphael <raphael@heynet.ch>
2025-12-08 15:07:20 +0100

    feat: Wrap sign-in progress indicator in LiquidBox
    Wraps the `LinearProgressIndicator` on the `SignInScreen` with a
    `LiquidBox` composable This is done to prevent the loss of the blur
    effect.

--- app/src/main/java/com/android/universe/ui/signIn/SignInScreen.kt
+++ app/src/main/java/com/android/universe/ui/signIn/SignInScreen.kt
@@ -37,6 +37,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -99,7 +100,9 @@ fun SignInScreen(
           modifier = Modifier.fillMaxSize(),
           verticalArrangement = Arrangement.Center,
           horizontalAlignment = Alignment.CenterHorizontally) {
-            LinearProgressIndicator()
+            LiquidBox(modifier = Modifier.wrapContentSize(), color = Color.Transparent) {
+              LinearProgressIndicator()
+            }
           }
   else
       when (uiState.onboardingState) {
