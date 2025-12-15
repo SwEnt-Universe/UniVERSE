178a2f3e67adbc5d266e2a9bf3670220c54f40c5
Noe.Das <noe.dassy@gmail.com>
2025-12-15 01:26:19 +0100

    refactor: Replace if-else with when for map mode in `MapScreen`
    Convert the `if-else if-else` block that determines the bottom bar content to a `when` expression for improved readability.

--- app/src/main/java/com/android/universe/ui/map/MapScreen.kt
+++ app/src/main/java/com/android/universe/ui/map/MapScreen.kt
@@ -225,7 +225,25 @@ fun MapScreen(
                             viewModel.switchMapMode(MapMode.NORMAL)
                           },
                           enabled = uiState.selectedLocation != null)))
-        } else {
+        } else if (uiState.mapMode == MapMode.CHANGE_LOCATION){
+            FlowBottomMenu(
+                flowTabs =
+                    listOf(
+                        FlowTab.Back(
+                            onClick = {
+                                onBackChangeLocation()
+                                viewModel.switchMapMode(MapMode.NORMAL)
+                            }),
+                        FlowTab.Confirm(
+                            onClick = {
+                                viewModel.resetFilter()
+                                viewModel.updateLocation()
+                                onBackChangeLocation()
+                                viewModel.switchMapMode(MapMode.NORMAL)
+                            },
+                            enabled = uiState.selectedLocation != null)))
+        }
+        else {
           FlowBottomMenu(
               flowTabs =
                   listOf(
