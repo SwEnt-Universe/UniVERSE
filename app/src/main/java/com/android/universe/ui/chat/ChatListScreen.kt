899682382dd7bb4234ba2241eaa69bce659a4392
hanslp <hanslp@stud.ntnu.no>
2025-12-17 13:03:06 +0100

    cleanup: merge with main

--- app/src/main/java/com/android/universe/ui/chat/ChatListScreen.kt
+++ app/src/main/java/com/android/universe/ui/chat/ChatListScreen.kt
@@ -1,6 +1,5 @@
 package com.android.universe.ui.chat
 
-import android.R.attr.maxHeight
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.BoxWithConstraints
